c824cab126065adad9e8e1f24c3992311d236dd8
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-08-01 14:15:41 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 288 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            if (s.equals("ENTER")) {
+                set.clear();
+            } else {
+                if (set.contains(s)) continue;
+                set.add(s);
+                count++;
+            }
+        }
+		
+		System.out.println(count);
+	}
+}
