e0dafd4299b7274bcf38aff615555f669c5da4f6
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-08-04 22:07:43 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r\'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
