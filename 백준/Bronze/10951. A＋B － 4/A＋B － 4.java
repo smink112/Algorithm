df6ab01291b39b805078a777f3faf91bc50f600d
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-08-04 22:18:47 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static StringTokenizer st;
+	public static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		String str;
+		sb = new StringBuilder();
+
+		while ((str = br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A + B).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
