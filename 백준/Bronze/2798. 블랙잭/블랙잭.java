a3564fe2a7ac640c205f56269a0c2c1cf9dcfc17
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-08-10 15:31:17 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static StringTokenizer st;
+	public static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int best = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				for (int k = j+1; k < arr.length; k++) {
+					int sum = arr[i]+arr[j]+arr[k];
+					if (M-best >= M- sum && M-sum>=0) {
+						best = sum;
+					}
+				}
+			}
+		}
+		
+		System.out.println(best);
+		
+	}
+}
