63aa2578806e510791340fe93fa2cb8d8c399980
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-09-04 09:37:02 +0900

    [Silver II] Title: N과 M (9), Time: 788 ms, Memory: 41688 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static StringTokenizer st;
+	static int N, r;
+	static boolean[] visited;
+	static LinkedHashSet<String> ans;
+
+	public static void main(String[] args) throws IOException {
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+		int[] out = new int[r];
+		visited = new boolean[N];
+		
+		ans = new LinkedHashSet<String>();	//처음보는거!
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		permutation(arr, out, 0, 0, r);
+		
+		ans.forEach(System.out::println);	//이것도 처음보넹
+
+	}
+
+	public static void permutation(int[] arr, int[] out, int start, int depth, int r) {
+		if (depth == r) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < out.length; i++) {
+				sb.append(out[i]).append(" ");
+			}
+			ans.add(sb.toString());
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				out[depth] = arr[i];
+				permutation(arr, out, i, depth + 1, r);
+				visited[i] = false;
+			}
+		}
+	}
+}
