6d9a58513b4bac7661a88d943ad53eb3bd170661
SeungminKo <95391402+smink112@users.noreply.github.com>
2023-10-03 22:35:36 +0900

    [Silver IV] Title: 설탕 배달, Time: 84 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,34 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int plasticBag5 = 0, plasticBag3 = 0;
-	
-	public static void main(String[] args) throws Exception {
-		int N = Integer.parseInt(br.readLine());
-		
-		System.out.println(bagCount(N));
-		
-	}
-	
-	public static int bagCount(int N) {
-		int result = 5000;
-		boolean b = false;
-		
-		for (int i = 0; i * 5 <= N; i++) {
-			for (int j = 0; j * 3 <= N; j++) {
-				if( i*5 + j*3 == N) {
-					result = Math.min(result, i+j);
-					b = true;
-				}
-			}
-		}
-		if(!b && result == 5000) result = -1;
-		return result;
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        if(N<5) {
+            if(N==3) System.out.println(1);
+            else System.out.println(-1);
+            return;
+        }
+        int[] dp = new int[N+1];
+        Arrays.fill(dp, 5001);
+        dp[3] = 1;
+        dp[5] = 1;
+        for(int i=6; i<dp.length; i++) {
+            //3키로 짜리를 써서 지금의 무게가 되는 경우와
+            //5키로 짜리를 써서 지금의 무게가 되는 경우
+            //두가지를 비교해서 더 적은 횟수가 지금 dp[무게]의 값
+            dp[i] = Math.min(dp[i-3]+1, dp[i-5]+1);
+        }
+        if(dp[N] > 5001) {
+            System.out.println(-1);
+        }else {
+            System.out.println(dp[N]);
+        }
+    }
 }
