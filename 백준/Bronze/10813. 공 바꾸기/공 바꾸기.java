af5ad66021057508c770d18f429a701d3d31d4d6
SeungminKo <95391402+smink112@users.noreply.github.com>
2024-02-20 10:16:13 +0900

    [Bronze II] Title: 공 바꾸기, Time: 96 ms, Memory: 12076 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int bucket[] = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            bucket[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int temp = bucket[a];
+            bucket[a] = bucket[b];
+            bucket[b] = temp;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.printf("%d ",bucket[i]);
+        }
+    }
+}
