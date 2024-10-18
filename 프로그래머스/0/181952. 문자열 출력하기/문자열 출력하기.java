d46e201019424ec37debdea6227447587e66515f
SeungminKo <95391402+smink112@users.noreply.github.com>
2024-10-19 00:05:19 +0900

    [level 0] Title: 문자열 출력하기, Time: 115.48 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine();
+
+        System.out.print(str);
+    }
+}
