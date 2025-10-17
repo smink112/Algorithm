51ab59f9f67bd3c85599046fb6364ed61f3e38a8
SeungminKo <95391402+smink112@users.noreply.github.com>
2025-10-17 20:56:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,31 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        int sum = Integer.MAX_VALUE;
-        // 뺄셈 기준으로 분리
-        st = new StringTokenizer(br.readLine(), "-");
+        st = new StringTokenizer(br.readLine());
+        int answer = 0;
 
-        while (st.hasMoreTokens()) {
-            int temp = 0;
+        String[] str = st.nextToken().split("-");
 
-            // 뺄셈으로 나뉜 토큰을 덧셈으로 분리하여 해당 토큰들을 더하기
-            StringTokenizer addition = new StringTokenizer(st.nextToken(), "+");
-            while (addition.hasMoreTokens()) {
-                temp += Integer.parseInt(addition.nextToken());
-            }
-
-            // 첫 번째토큰인 경우 temp값이 첫 번째 수가 됨
-            if (sum == Integer.MAX_VALUE) {
-                sum = temp;
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i == 0) {
+                answer += temp;
             } else {
-                sum -= temp;
+                answer -= temp;
             }
         }
-        System.out.println(sum);
+
+        System.out.println(answer);
+    }
+
+    static int mySum(String s) {
+        int sum = 0;
+        String[] temp = s.split("[+]");
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+
+        return sum;
     }
 }
