5b66ef6446ebb9b323f2159ef8b5db443f5acbdf
SeungminKo <95391402+smink112@users.noreply.github.com>
2025-10-25 21:04:08 +0900

    [Gold V] Title: 최소비용 구하기, Time: 392 ms, Memory: 51808 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,81 +7,74 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringTokenizer st;
-    public static StringBuilder sb;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M, start, end;
 
-    static int N, M, start, desti;
-    static int[] dist;
-    static ArrayList<Node>[] list;
+    static int[] distance;
     static boolean[] visited;
+    static PriorityQueue<BusStop> pq = new PriorityQueue<>();
+    static ArrayList<BusStop>[] list;
 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-
+        distance = new int[N + 1];
+        visited = new boolean[N + 1];
         list = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        for (int i = 1; i < N + 1; i++) {
             list[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             int v = Integer.parseInt(st.nextToken());
             int u = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            list[v].add(new Node(u, c));
+            int d = Integer.parseInt(st.nextToken());
+
+            list[v].add(new BusStop(u, d));
         }
 
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         start = Integer.parseInt(st.nextToken());
-        desti = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
 
-        dist = new int[N + 1];
-        visited = new boolean[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        pq.add(new BusStop(start, 0));
+        distance[start] = 0;
+        
+        while (!pq.isEmpty()) {
+            BusStop nowNode = pq.poll();
+            int now = nowNode.nextStop;
 
-        dijkstra(start);
+            if (visited[now]) continue;
+            visited[now] = true;
 
-
-        System.out.println(dist[desti]);
-    }
-
-    public static void dijkstra(int start) {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        dist[start] = 0;
-        q.offer(new Node(start, 0));
-
-        while (!q.isEmpty()) {
-            Node current = q.poll();
-
-            if (!visited[current.v])
-                visited[current.v] = true;
-            else
-                continue;
-
-            for (int i = 0; i < list[current.v].size(); i++) {
-                Node next = list[current.v].get(i);
-                if (dist[next.v] > dist[current.v] + next.cost) {
-                    dist[next.v] = dist[current.v] + next.cost;
-                    q.offer(new Node(next.v, dist[next.v]));
+            for (BusStop stop : list[now]) {
+                if (!visited[stop.nextStop] && distance[stop.nextStop] > distance[now] + stop.value) {
+                    distance[stop.nextStop] = distance[now] + stop.value;
+                    pq.add(new BusStop(stop.nextStop, distance[stop.nextStop]));
                 }
             }
         }
-    }// dijkstra
 
-    public static class Node implements Comparable<Node> {
-        int v;
-        int cost;
+        System.out.println(distance[end]);
+    }
 
-        Node(int v, int cost) {
-            this.v = v;
-            this.cost = cost;
-        }
+}
 
-        @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
-        }
-    }//Node
+class BusStop implements Comparable<BusStop> {
+    int nextStop, value;
+
+    BusStop(int vertex, int value) {
+        this.nextStop = vertex;
+        this.value = value;
+    }
+
+    public int compareTo(BusStop o) {
+        if (this.value > o.value) return 1;
+        else return -1;
+    }
 }
